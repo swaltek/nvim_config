2d73fc70a3b4db545d3f2213def95077173aef6c
Marcin Swaltek <swaltek@tutanota.com>
2023-05-15 15:43:55 +0200

    added color change function

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,1 +1,10 @@
 vim.cmd('colorscheme tokyonight-moon')
+local light_mode = false;
+function ToggleLight()
+	light_mode = not light_mode;
+	if light_mode then
+		vim.cmd('colorscheme tokyonight-day')
+	else
+		vim.cmd('colorscheme tokyonight-moon')
+	end
+end
